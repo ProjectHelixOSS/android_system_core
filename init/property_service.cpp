ProjectLisa-AOSP/system_core

[development-support] adding developer debug hooks
Change-Id: I2a20f288a60690ab66d1e2f9b6b30ba21f0cca3e
Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

init/property_service.cpp
@@ -1254,6 +1254,35 @@ void LoadReleaseBuildProperties() {
     }
 }
 
+void LoadDebugProperties() {
+    const char* DEBUG_PROP = "persist.sys.ax_debug_enabled";
+    std::string error;
+    uint32_t res;
+
+    std::string debug_value = GetProperty(DEBUG_PROP, "1");
+
+    if (debug_value == "1") {
+        const std::pair<const char*, const char*> debug_props[] = {
+            {"service.adb.root", "1"},
+            {"ro.adb.secure", "0"},
+            {"ro.debuggable", "1"},
+            {"ro.force.debuggable", "1"},
+            {"persist.sys.usb.config", "adb"},
+            {"sys.usb.config", "adb"}
+        };
+
+        for (const auto& [name, value] : debug_props) {
+            res = PropertySetNoSocket(name, value, &error);
+            if (res == PROP_SUCCESS) {
+                LOG(INFO) << "Debug property '" << name << "' set to '" << value << "'";
+            } else {
+                LOG(ERROR) << "Failed to set debug property '" << name
+                           << "' to '" << value << "': err=" << res << " (" << error << ")";
+            }
+        }
+    }
+}
+
 void PropertyLoadBootDefaults() {
     // We read the properties and their values into a map, in order to always allow properties
     // loaded in the later property files to override the properties in loaded in the earlier
@@ -1370,6 +1399,7 @@ void PropertyLoadBootDefaults() {
       }
     }
 
+    LoadDebugProperties();
     LoadReleaseBuildProperties();
 
     // Restore the normal property override security after init extension is executed

ProjectHelixOSS/android_system_core

init/property_service.cpp
@@ -1268,6 +1268,35 @@ void LoadVbMetaOverrides() {
     }
 }
 
+void LoadDebugProperties() {
+    const char* DEBUG_PROP = "persist.sys.helix_debug_enabled";
+    std::string error;
+    uint32_t res;
+
+    std::string debug_value = GetProperty(DEBUG_PROP, "1");
+
+    if (debug_value == "1") {
+        const std::pair<const char*, const char*> debug_props[] = {
+            {"service.adb.root", "1"},
+            {"ro.adb.secure", "0"},
+            {"ro.debuggable", "1"},
+            {"ro.force.debuggable", "1"},
+            {"persist.sys.usb.config", "adb"},
+            {"sys.usb.config", "adb"}
+        };
+
+        for (const auto& [name, value] : debug_props) {
+            res = PropertySetNoSocket(name, value, &error);
+            if (res == PROP_SUCCESS) {
+                LOG(INFO) << "Debug property '" << name << "' set to '" << value << "'";
+            } else {
+                LOG(ERROR) << "Failed to set debug property '" << name
+                           << "' to '" << value << "': err=" << res << " (" << error << ")";
+            }
+        }
+    }
+}
+
 void PropertyLoadBootDefaults() {
     // We read the properties and their values into a map, in order to always allow properties
     // loaded in the later property files to override the properties in loaded in the earlier
@@ -1603,6 +1632,8 @@ static void HandleInitSocket() {
                 // Always enable usb adb if device is booted with debug ramdisk.
                 update_sys_usb_config();
             }
+
+	    LoadDebugProperties();
 
 	    LoadVbMetaOverrides();
 